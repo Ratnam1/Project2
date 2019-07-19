9efb54afff089f0ad59914856ff43766808c02ac
Cloud User <ec2-user@ip-172-31-16-181.us-east-2.compute.internal>
2019-07-19 14:48:57 +0000

    Java Files

---
+++ src/ExampleProgram.java
@@ -0,0 +1,6 @@
+class ExampleProgram {
+  public static void main(String[] args){
+    System.out.println("Simple Java source code to demonstrate\ncommandline or manual compilation process");
+  }
+}
+
